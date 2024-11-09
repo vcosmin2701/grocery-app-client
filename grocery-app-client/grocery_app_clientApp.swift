c8a846f6cd53a534898e14e5dba317a9544a177b
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-09 23:05:08 +0200

    Initial Commit

---
+++ grocery-app-client/grocery_app_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocery_app_clientApp.swift
+//  grocery-app-client
+//
+//  Created by Vladut Cosmin on 09.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct grocery_app_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
