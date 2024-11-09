5cc6ada032cd1966e7f2a805f07d4c98b5b3e07e
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-09 23:35:11 +0200

    Registration Screen - Simple form with constraints

---
+++ grocery-app-client/Screens/RegistrationScreen.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+struct RegistrationScreen: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    private var isFormValid: Bool {
+        !username.isEmptyOrWhiteSpace && !password.isEmptyOrWhiteSpace && (password.count >= 6 && password.count <= 12)
+    }
+    
+    var body: some View {
+        Form {
+            TextField("Username", text: $username)
+            SecureField("Password", text: $password)
+            
+            HStack {
+                Button("Register"){
+                    
+                }.buttonStyle(.borderless)
+                    .disabled(!isFormValid)
+            }
+        }
+    }
+}
+
+#Preview {
+    RegistrationScreen()
+}
